84aa02c20b22f1e9f2124b840386c91f08b9ff6b
sunxiaojie79 <297280911@qq.com>
2020-07-05 00:34:00 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ const baseConfig = require('../../lib/webpack.base.js');
 describe('webpack.base.js test case', () => {
   // eslint-disable-next-line no-undef
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/sunjie/Desktop/webpack-study/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/sunjie/Desktop/webpack-study/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('webpack-builder-sj/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('webpack-builder-sj/test/smoke/template/src/search/index.js') > -1, true );
   });
 });
