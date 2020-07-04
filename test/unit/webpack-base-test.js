c26fa2383b350bdd869f50b49ccb9b9a477142da
sunxiaojie79 <297280911@qq.com>
2020-07-05 00:37:18 +0800

    feat:持续集成到travis ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,6 +6,6 @@ describe('webpack.base.js test case', () => {
   // eslint-disable-next-line no-undef
   it('entry', () => {
     assert.equal(baseConfig.entry.index.indexOf('webpack-builder-sj/test/smoke/template/src/index/index.js') > -1, true);
-    assert.equal(baseConfig.entry.search.indexOf('webpack-builder-sj/test/smoke/template/src/search/index.js') > -1, true );
+    assert.equal(baseConfig.entry.search.indexOf('webpack-builder-sj/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
